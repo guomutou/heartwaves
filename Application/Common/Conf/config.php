93722d57e53b28ba72f7e5df950517d983e05aa9
guobin <1406630169@qq.com>
2018-03-30 16:37:44 +0800

    修改配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
     //'配置项'=>'配置值'
         'DB_TYPE'   => 'mysqli', // 数据库类型
-        'DB_HOST'   => '127.0.0.1', // 服务器地址
+        'DB_HOST'   => '172.21.0.9', // 服务器地址
         'DB_NAME'   => 'heartwaves', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'haofengpmts',  // 密码
+        'DB_PWD'    => 'Hrv123456',  // 密码
         'DB_PORT'   => '3306', // 端口
         'DB_PREFIX' => 'h_', // 数据库表前缀
         'SHOW_PAGE_TRACE' =>false,  //
